0f8947b1b13904bfac9d84c0fc4cac650423921b
oluwapelumi1 <oluwapelumidaniel88@gmail.com>
2023-05-29 12:31:48 +0100

    fix

---
+++ src/scenes/calendar/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useState } from 'react'
+import FullCalendar, { formatDate } from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import timeGridPlugin from '@fullcalendar/timegrid'
+import InteractionPlugin from '@fullcalendar/interaction'
+import listPlugin from '@fullcalendar/list'
+import { Box, List, ListItem, Typography, useTheme } from '@mui/materials'
+import Header from '../../components/Header'
+import { token } from '../../theme'
+
+const Calendar = () => {
+  return <div>index</div>
+}
+
+export default index
